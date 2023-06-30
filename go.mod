8d2953c179773d3f3d9069e48e08bfb3c43d500f
ilya <18560147+andreev-io@users.noreply.github.com>
2023-06-30 21:09:25 +0100

    locally working draft

---
+++ go.mod
@@ -0,0 +1,46 @@
+module cofin
+
+go 1.20
+
+require (
+	github.com/dslipak/pdf v0.0.1
+	github.com/joho/godotenv v1.5.1
+	github.com/tmc/langchaingo v0.0.0-20230630075547-a90d3dfb104f
+)
+
+require (
+	cloud.google.com/go v0.110.0 // indirect
+	cloud.google.com/go/aiplatform v1.42.0 // indirect
+	cloud.google.com/go/compute v1.19.0 // indirect
+	cloud.google.com/go/compute/metadata v0.2.3 // indirect
+	cloud.google.com/go/iam v1.0.1 // indirect
+	cloud.google.com/go/longrunning v0.4.2 // indirect
+	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/aymerick/douceur v0.2.0 // indirect
+	github.com/dlclark/regexp2 v1.10.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/s2a-go v0.1.3 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.2.3 // indirect
+	github.com/googleapis/gax-go/v2 v2.8.0 // indirect
+	github.com/gorilla/css v1.0.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.7.2 // indirect
+	github.com/microcosm-cc/bluemonday v1.0.24 // indirect
+	github.com/pinecone-io/go-pinecone v0.3.0 // indirect
+	github.com/pkoukk/tiktoken-go v0.1.4 // indirect
+	go.opencensus.io v0.24.0 // indirect
+	golang.org/x/crypto v0.7.0 // indirect
+	golang.org/x/exp v0.0.0-20220303212507-bbda1eaf7a17 // indirect
+	golang.org/x/net v0.10.0 // indirect
+	golang.org/x/oauth2 v0.8.0 // indirect
+	golang.org/x/sys v0.8.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	google.golang.org/api v0.122.0 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20230410155749-daa745c078e1 // indirect
+	google.golang.org/grpc v1.55.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+
+	"github.com/dslipak/pdf"
+	"github.com/joho/godotenv"
+	loader "github.com/tmc/langchaingo/documentloaders"
+	"github.com/tmc/langchaingo/embeddings"
+	"github.com/tmc/langchaingo/llms/openai"
+	"github.com/tmc/langchaingo/schema"
+	"github.com/tmc/langchaingo/vectorstores/pinecone"
+)
+
+func main() {
+	godotenv.Load()
+
+	content, err := readPdf("cloudflare-10-q-march-2023.pdf")
+	if err != nil {
+		panic(err)
+	}
+
+	text := loader.NewText(strings.NewReader(content))
+	splitter, err := NewSplitter(1000, 100)
+	if err != nil {
+		panic(err)
+	}
+
+	docs, err := text.LoadAndSplit(context.Background(), splitter)
+	if err != nil {
+		panic(err)
+	}
+
+	embedder, err := embeddings.NewOpenAI()
+	embedder.BatchSize = 30
+
+	store, err := pinecone.New(
+		context.Background(),
+		pinecone.WithProjectName(os.Getenv("PINECONE_PROJECT")),
+		pinecone.WithIndexName(os.Getenv("PINECONE_INDEX")),
+		pinecone.WithEnvironment(os.Getenv("PINECONE_ENVIRONMENT")),
+		pinecone.WithEmbedder(embedder),
+		pinecone.WithAPIKey(os.Getenv("PINECONE_API_KEY")),
+		pinecone.WithNameSpace("$NET"),
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Go in batches of 15 when pushing to Pinecone.
+	for i := 0; i <= len(docs); i += 30 {
+		end := i + 15
+		if end > len(docs) {
+			end = len(docs)
+		}
+
+		// err = store.AddDocuments(context.Background(), docs[i:end])
+		// if err != nil {
+		// log.Fatal(err)
+		// }
+	}
+
+	userMessage := "Hey, who is the CFO of Cloudflare?"
+
+	docs, err = store.SimilaritySearch(context.Background(), userMessage, 5)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	docStrings := make([]string, len(docs))
+	for i, doc := range docs {
+		docStrings[i] = doc.PageContent
+	}
+	// log.Println(strings.Join(docStrings, "\n"))
+
+	// TODO: calculate token limit
+	// tkm, err := tiktoken.EncodingForModel("gpt-3.5-turbo")
+	chat, err := openai.NewChat(openai.WithModel("gpt-3.5-turbo"))
+	res, err := chat.Call(context.Background(), []schema.ChatMessage{
+		schema.SystemChatMessage{
+			Text: `
+			You are Artificial Intelligence built purposely to analyse financial filings of publicly traded companies and answer questions based on them.
+			Below you will find a list of paragraphs from the 10-Q filing of Cloudflare, Inc. for the quarter ended March 31, 2023.
+			Please read them and answer the user message below. You can optionally use the provided paragraph as context to answer the user.
+			You may choose not to use the paragraphs in your answer.
+			Think step by step and explain your reasoning.
+			`,
+		},
+		schema.AIChatMessage{
+			Text: `Understood.`,
+		},
+		schema.SystemChatMessage{
+			Text: fmt.Sprintf("Here are the paragraphs from Cloudflare's ($NET) 10-Q filing from March 31, 2023 most relevant to user's input:\n%v", strings.Join(docStrings, "\n")),
+		},
+		schema.AIChatMessage{
+			Text: `Sounds good.`,
+		},
+		schema.HumanChatMessage{
+			Text: userMessage,
+		},
+	})
+
+	log.Printf("Question: %v\n\nAnswer: %v\n", userMessage, res)
+}
+
+func readPdf(path string) (string, error) {
+	r, err := pdf.Open(path)
+	if err != nil {
+		return "", err
+	}
+
+	var buf bytes.Buffer
+	b, err := r.GetPlainText()
+	if err != nil {
+		return "", err
+	}
+
+	buf.ReadFrom(b)
+	return buf.String(), nil
+}
